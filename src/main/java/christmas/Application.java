9db059f08d67122203c2baf1a617c23779c14e08
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-13 19:41:13 +0900

    feat: 입력을 위한 출력 메시지

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -5,5 +5,6 @@ import javax.naming.ldap.Control;
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
+        controller.Start();
     }
 }
--- src/main/java/christmas/Controller.java
+++ src/main/java/christmas/Controller.java
@@ -1,4 +1,10 @@
 package christmas;
 
 public class Controller {
+    public void Start() {
+        View view = new View();
+        view.StartMessage();
+        view.EnterMenuMessage();
+        view.PreviewEventBenefitMessage(26);
+    }
 }
