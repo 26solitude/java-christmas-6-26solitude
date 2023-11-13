33f73de9c161dabdd48d9f7df700e9b96a790d3c
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-13 18:35:56 +0900

    feat: MVC 분리

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,9 @@
 package christmas;
 
+import javax.naming.ldap.Control;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
     }
 }
